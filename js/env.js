5485f9a0b8d5c46de458dc0a89925c133e231cdc
Dhinagitcoin <108085379+Dhinagitcoin@users.noreply.github.com>
2023-05-14 10:38:26 +0530

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const nftAddress = "0x903f0A64258a6A7b2e2aDB6C8261D1B3611A9c93"; // Paste the address of the NFT collection.
+const nftAddress = "0x9cf390ae8293e39cf539975d70ef884dc89103f4"; // Paste the address of the NFT collection.
 // uncomment the chain you are using:
 const chainId = "0x507"; // Moonbase
 // const chainId = "0x504"; // Moonbeam
